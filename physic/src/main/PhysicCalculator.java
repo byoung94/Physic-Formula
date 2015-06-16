612982fd3f21d0d17e116960330bca437feee541
byoung94 <boony4ng94@gmail.com>
2015-06-16 10:40:55 +0800

    I add Force formula

---
+++ physic/src/main/PhysicCalculator.java
@@ -0,0 +1,10 @@
+package main;
+
+public class PhysicCalculator {
+	
+
+	public static double force(double mass, double acceleration){
+		return mass*acceleration;
+	}
+
+}
---
+++ physic/src/main/PhysicMain.java
@@ -0,0 +1,10 @@
+package main;
+
+
+public class PhysicMain{
+
+	public static void main(String[] args) {
+
+	}
+
+}
